49b1fc0c970ee48007e77fc0a36499fbe087dd64
akilaanjanadissanayaka <akiladissanayaka255@gmail.com>
2022-11-14 12:05:12 +0530

    initialize the project

---
+++ src/main/java/com/example/helloworld/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.example.helloworld;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @RequestMapping
+    public String helloWorld(){
+        return "Hello world from spring boot";
+    }
+//    Alt+Shift+F10 to run
+    @RequestMapping("/goodbye")
+    public String goodbay(){
+        return "Bye Byeeee";
+    }
+}
