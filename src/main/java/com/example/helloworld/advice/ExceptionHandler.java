438ecce6cb337dfa1eff98108ad8ba8b27cc86fc
akilaanjanadissanayaka <akiladissanayaka255@gmail.com>
2022-11-24 15:09:33 +0530

    slf4j

--- src/main/java/com/example/helloworld/advice/ExceptionHandler.java
+++ src/main/java/com/example/helloworld/advice/ExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.example.helloworld.advice;
 
+import com.example.helloworld.controller.productController;
 import com.example.helloworld.exception.ProductNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -13,7 +16,7 @@ import java.util.Map;
 
 @RestControllerAdvice
 public class ExceptionHandler {
-
+    Logger logger = LoggerFactory.getLogger(productController.class);
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @org.springframework.web.bind.annotation.ExceptionHandler(MethodArgumentNotValidException.class)
     public Map<String, String> handleValidationExceptions(
@@ -24,6 +27,7 @@ public class ExceptionHandler {
             String errorMessage = error.getDefaultMessage();
             errors.put(fieldName, errorMessage);
         });
+        logger.info("error -"+errors);
         return errors;
     }
 
@@ -34,6 +38,7 @@ public class ExceptionHandler {
 
         Map<String, String> errors = new HashMap<>();
         errors.put("Error:", px.getMessage());
+
         return  errors;
     }
 }
